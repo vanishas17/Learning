bde50f642f1c4521955f8cb3b4ba8d6271abf026
vanishas17 <vanisha.sawant@gmail.com>
2021-05-25 16:10:49 -0400

    Adding new folder

---
+++ ExtensionMethods/ExtensionMethods/MyLinq.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public static class MyLinq
+    {
+        public static int Count<T>(this IEnumerable<T> sequence)
+        {
+            int count = 0;
+            foreach(var item in sequence)
+            {
+                count++;
+            }
+            return count;
+        }
+    }
+}
---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Employee> developers = new Employee[]
+            {
+                new Employee {Id = 1, Name="Emp 1" },
+                new Employee {Id = 2, Name="Emp 2" }
+            };
+
+            IEnumerable<Employee> testers = new List<Employee>()
+            {
+                new Employee {Id = 3, Name="Tester 1" },
+                new Employee {Id = 4, Name="Tester 2" }
+            };
+
+            Console.WriteLine(developers.Count());
+
+        }
+    }
+}
