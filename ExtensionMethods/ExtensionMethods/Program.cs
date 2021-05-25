62b59592d873baf3e8628f46ed3911a71373a3c9
vanishas17 <vanisha.sawant@gmail.com>
2021-05-25 18:58:26 -0400

    Lambda expressions

--- ExtensionMethods/ExtensionMethods/Program.cs
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace ExtensionMethods
 {
@@ -23,6 +24,62 @@ namespace ExtensionMethods
 
             Console.WriteLine(developers.Count());
 
+            //================Lambda Expressions=========================//
+
+            //Named Methods
+            foreach (var emp in developers.Where(NameStartsWithE))
+            {
+                Console.WriteLine(emp.Name);
+            }
+
+            //Anonymous methods
+            foreach (var emp in developers.Where(
+                delegate (Employee employee)
+                {
+                    return employee.Name.StartsWith("E");
+                }))
+            {
+                Console.WriteLine(emp.Name);
+            }
+
+            //Lambda Expresions
+            foreach (var emp in developers.Where(e => e.Name.StartsWith("E")))                
+            {
+                Console.WriteLine(emp.Name);
+            }
+
+            //Func and Action
+            Func<int, int> square = x => x * x;
+            Console.WriteLine("Square is : " + square(3));
+
+            Func<int, int, int> add = (x, y) => x + y;
+            Console.WriteLine("Sum is: "+ add(3, 5));
+
+            Func<int, int, int> addnum = (x, y) => {
+                int sum = x + y;
+                return sum;
+            };
+
+            Console.WriteLine("Sum of numbers is: " + add(3, 5));
+
+
+
+            //Action returns always void
+            Action<int> write =  x => Console.WriteLine("final : " + x);
+            write(square(add(3, 5)));
+
+            Console.Read();
+
         }
+
+
+
+        //Named Methods
+        private static bool NameStartsWithE(Employee employee)
+        {
+            return employee.Name.StartsWith("E");
+        }
+
+
     }
 }
