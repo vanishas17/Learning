0e186c74ef703490564b9a6d7a482959a1ae1e7c
vanishas17 <vanisha.sawant@gmail.com>
2021-06-02 15:56:27 -0400

    Unit of work

--- ExpenseApprovalAPI/Libraries/ExpenseApproval.DataAccess/Repository/Wrapper/RepositoryWrapper.cs
+++ ExpenseApprovalAPI/Libraries/ExpenseApproval.DataAccess/Repository/Wrapper/RepositoryWrapper.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace ExpenseApproval.DataAccess.Repository
 {
-    public class RepositoryWrapper : IRepositoryWrapper
+    public class RepositoryWrapper : IRepositoryWrapper, IDisposable
     {
         private ExpenseDataContext _context;
 
@@ -55,11 +55,30 @@ namespace ExpenseApproval.DataAccess.Repository
                 return _expense;
             }
         }
-
-
+        
         public void Save()
         {
             _context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
     }
 }
